57835ef718dd4104774d52251f7ee0161a09739a
Eric Jonas <jonas@ericjonas.com>
2013-07-18 16:51:37 -0700

    actually add fast static vector

---
+++ irm/fast_static_vect.h
@@ -0,0 +1,64 @@
+#include <stdexcept>
+#pragma once
+
+template<typename T, int MAX_SIZE>
+class fast_static_vector {
+public:
+    fast_static_vector() : 
+        size_(0) 
+    { 
+
+    }
+
+    fast_static_vector(int size) :
+        size_(size)
+    { 
+        for(int i = 0; i < size; ++i) { 
+            data_[i] = 0; // fixme should be init type
+        }
+
+    }
+
+    fast_static_vector(int size, const T init_val) :
+        size_(size)
+    { 
+        for(int i = 0; i < size; ++i) { 
+            data_[i] = init_val; 
+
+        }
+    }
+
+    T & operator[](int pos){  
+
+        return data_[pos]; 
+    }
+
+    T operator[](int pos) const{  
+
+        return data_[pos]; 
+    }
+
+    bool operator <(const fast_static_vector<T, MAX_SIZE> & b) const { 
+        if(size_ == b.size_) { 
+            for(int i = 0; i < size_; ++i) { 
+                if (data_[i] < b.data_[i]) { 
+                    return false; 
+                } else if (data_[i] > b.data_[i]) { 
+                    return true; 
+                }
+            }
+            return true; 
+
+
+        } else { 
+            throw std::runtime_error("can't compare unequal size"); 
+        }
+
+    }
+
+
+private:
+    int size_; 
+    T data_[MAX_SIZE]; 
+
+}; 
