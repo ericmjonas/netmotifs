3216819ac00dcc24c2aec910bde1cd5f6d0929d1
Eric Jonas <jonas@ericjonas.com>
2013-07-19 11:46:55 -0700

    added sigmoid function; should test

--- experiments/logit.py
+++ experiments/logit.py
@@ -5,7 +5,10 @@ import numpy as np
 def logistic(x, mu, lamb):
     return 1.0/(1 + np.exp((x - mu)/lamb))
 
-def parametric_plot():
+def sigmoid(x, mu, lamb):
+    return -(x-mu) / (lamb + np.abs(x-mu))*0.5  + 0.5
+
+def parametric_plot(func):
     """
     Plot the example curves
     """
@@ -15,7 +18,7 @@ def parametric_plot():
     pylab.figure()
     for mu in np.linspace(0, 100, 8):
         lamb = 4.0
-        pylab.plot(x, logistic(x, mu, lamb), linewidth=3.0)
+        pylab.plot(x, func(x, mu, lamb), linewidth=3.0)
         #pylab.axvline(mu, linestyle='--')
     pylab.grid(1)
     pylab.axis([0, MAXX, 0, 1])
@@ -25,7 +28,7 @@ def parametric_plot():
     for c, mu in [('r', 0.0), ('b', 50.0)]:
 
         for lamb in [0.1, 1.0, 4.0, 10.0, 20, 50]:
-            pylab.plot(x, logistic(x, mu, lamb), linewidth=3, 
+            pylab.plot(x, func(x, mu, lamb), linewidth=3, 
                        c=c)
         #pylab.axvline(mu, linestyle='--')
     pylab.grid(1)
@@ -35,7 +38,7 @@ def parametric_plot():
     pylab.show()
 
 
-def prior_sample():
+def prior_sample(func):
     MAXX = 10.0
     x = np.linspace(0, MAXX, 1000)
 
@@ -44,11 +47,11 @@ def prior_sample():
     for i in range(10):
         mu = np.random.exponential(mu_hp)
         lamb = np.random.exponential(lamb_hp)
-        pylab.plot(x, logistic(x, mu, lamb), c='k', alpha=0.5, linewidth=2)
+        pylab.plot(x, func(x, mu, lamb), c='k', alpha=0.5, linewidth=2)
     pylab.show()
 
 
-def single_plot():
+def single_plot(func):
     MAXX = 5.0
     x = np.linspace(0, MAXX, 1000)
 
@@ -57,7 +60,7 @@ def single_plot():
     pmin = 0.1
     pmax = 0.9
     
-    y = logistic(x, mu, lamb)
+    y = func(x, mu, lamb)
     scaled_y = y * (pmax - pmin) + pmin
     
     pylab.subplot(1, 2, 1)
@@ -71,8 +74,8 @@ def single_plot():
     pylab.axis([0, MAXX, 0, 1])
     pylab.show()
 
-#parametric_plot()
-#prior_sample()
+parametric_plot(sigmoid)
+prior_sample(sigmoid)
 
 
-single_plot()
+#single_plot()
