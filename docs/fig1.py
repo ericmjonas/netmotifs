1975952db806494a0e853170133d0748af2437c7
Eric Jonas <jonas@ericjonas.com>
2014-05-12 16:38:50 -0400

    updates to pedagogy figs

--- docs/fig1.py
+++ docs/fig1.py
@@ -69,14 +69,94 @@ ax.imshow(c_r, interpolation='nearest', cmap=pylab.cm.Greys)
 
 ax.set_xticks([])
 ax.set_yticks([])
+f.savefig("source.f1.raw.pdf")
+
+cd = conn_and_dist['distance'][ai, :]
+cd = cd[:, ai]
+ax.imshow(cd, interpolation='nearest', cmap=pylab.cm.Greys)
+
+ax.set_xticks([])
+ax.set_yticks([])
+f.savefig("source.f1.distance.pdf")
+
 
 c_class = nodes_with_class['class']
+
+# now create synthetic distances for each class
+class_syn_dist = { 0 : (0.2, 0.5, 0.7), 
+                   1 : (0.3,), 
+                   2 : (0.55, 0.6), 
+                   3 : (0.8,), 
+                   4 : (0.1, 0.9)}
+class_soma_pos_var = { 0 : (0.1, 0.05), 
+                       1 : (0.3, 0.01), 
+                       2 : (0.5, 0.03), 
+                       3 : (0.85, 0.1), 
+                       4 : (0.3, 0.04)
+}
+
+N_SYN = 40
+syn_pro_bins = np.linspace(0, 1.0, 30)
+syn_pro_hist = np.zeros((len(c_class), len(syn_pro_bins)-1))
+soma_depth = np.zeros(len(c_class))
+
+for ci, c in enumerate(c_class):
+    pos = []
+    for i in range(N_SYN):
+        pi = np.random.randint(0, len(class_syn_dist[c]))
+        p = np.random.normal(class_syn_dist[c][pi], 0.07)
+        pos.append(p)
+    b, _= np.histogram(pos, bins=syn_pro_bins)
+    syn_pro_hist[ci] = b
+    mu, std = class_soma_pos_var[c]
+    soma_depth[ci] = np.random.normal(mu, std)
+
+f = pylab.figure()
+ax = f.add_subplot(1, 1, 1)
+ax.imshow(syn_pro_hist, interpolation='nearest', cmap=pylab.cm.Greys)
+
+ax.set_xticks([])
+ax.set_yticks([])
+f.savefig("source.f1.synprof.pdf")
+
+f = pylab.figure()
+ax = f.add_subplot(1, 1, 1)
+
+ax.imshow(syn_pro_hist[ai], interpolation='nearest', cmap=pylab.cm.Greys)
+
+ax.set_xticks([])
+ax.set_yticks([])
+f.savefig("source.f1.synprof.raw.pdf")
+
+
+f = pylab.figure(figsize=(3, 6))
+ax = f.add_subplot(1, 1, 1)
+ax.scatter(soma_depth, 
+           np.arange(len(c_class)), edgecolor='none', c='k')
+ax.set_xlim(0, 1)
+ax.set_ylim(len(c_class), 0)
+ax.set_xticks([])
+ax.set_yticks([])
+f.savefig("source.f1.somadepth.pdf")
+
+
+f = pylab.figure(figsize=(3, 6))
+ax = f.add_subplot(1, 1, 1)
+ax.scatter(soma_depth[ai], 
+           np.arange(len(c_class)), edgecolor='none', c='k')
+ax.set_xlim(0, 1)
+ax.set_ylim(len(c_class), 0)
+ax.set_xticks([])
+ax.set_yticks([])
+f.savefig("source.f1.somadepth.raw.pdf")
+
+
+
 CLASS_N = len(np.unique(c_class))
 
 ai = np.argsort(c_class).flatten()
 c_sorted = connectivity[ai, :]
 c_sorted = c_sorted[:, ai]
-f.savefig("source.f1.raw.pdf")
 
 f2 = pylab.figure()
 ax = f2.add_subplot(1, 1, 1)
@@ -114,6 +194,68 @@ f3 = pylab.figure()
 irm.plot.plot_t1t1_params(f3, conn_and_dist, nodes_with_class['class'], 
                           ss, hps, model="LogisticDistance", MAX_DIST=3.5)
 f3.savefig("source.f1.latent.pdf")
+
+
+DIST = 0.5
+
+circos_p = irm.plots.circos.CircosPlot(c_class, "0.5r", "70p", 
+                                           ['red', 'green', 'blue', 'yellow', 'purple'])
+
+v = irm.irmio.latent_distance_eval(DIST, 
+                                   ss, hps, 'LogisticDistance')
+
+thold = 0.3
+ribbons = []
+links = []
+pairs_plotted = set()
+
+ribbons = []
+for (src, dest) in v.keys():
+    p1 = v[(src, dest)]
+    p2 = v[(dest, src)]
+    p = max(p1, p2)
+    if (src, dest) in pairs_plotted or (dest, src) in pairs_plotted:
+        pass
+    else:
+        if p > thold :
+            pix = int(10*p)
+            print src, dest, p, pix
+
+            ribbons.append((src, dest, pix))
+
+    pairs_plotted.add((src, dest))
+
+circos_p.add_class_ribbons(ribbons)
+circos_p.add_plot('scatter', {'r0' : '1.0r', 
+                              'r1' : '1.15r', 
+                              'min' : 0, 
+                              'max' : 1., 
+                              'glyph' : 'circle', 
+                              'glyph_size' : 12, 
+                              'color' : 'black',
+                              'stroke_thickness' : 0
+                          }, 
+                  soma_depth, 
+                  {'backgrounds' : [('background', {'color': 'vvlgrey', 
+                                                    'y0' : 0.0, 
+                                                    'y1' : 1.0})],  
+                   'axes': [('axis', {'color' : 'vgrey', 
+                                      'thickness' : 1, 
+                                      'spacing' : '%fr' % 0.1})]})
+
+
+for bi, b in enumerate(syn_pro_bins[:-1]):
+    width = 0.15/20.
+    start = 1.15 + width*bi
+    end = start + width
+    circos_p.add_plot('heatmap', {'r0' : '%fr' % start, 
+                                  'r1' : '%fr' % end, 
+                                  'stroke_thickness' : 0, 
+                                  'color' : 'greys-6-seq'}, 
+                      syn_pro_hist[:, bi])
+
+irm.plots.circos.write(circos_p, "source.f1.circos.png")
+
 
 DISTS = [0.1, 1.0, 2.5]
 for dist_i, dist_threshold in enumerate(DISTS):
